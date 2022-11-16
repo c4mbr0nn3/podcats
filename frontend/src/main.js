87c33961030536996e35178d9aab216c827d4fea
Francesco Zorzi <frensinik89@yahoo.it>
2022-11-16 22:33:34 +0100

    add vuetify 3 frontend

---
+++ frontend/src/main.js
@@ -0,0 +1,23 @@
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Components
+import App from './App.vue'
+
+// Composables
+import { createApp } from 'vue'
+
+// Plugins
+import { registerPlugins } from '@/plugins'
+import vuetify from './plugins/vuetify'
+
+const app = createApp(App)
+
+registerPlugins(app)
+
+app
+  .use(vuetify)
+  .mount('#app')
