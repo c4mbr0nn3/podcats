a50c759c84dc541c6fe81e20d9e0a65b87011fef
Francesco Zorzi <frensinik89@yahoo.it>
2022-12-09 19:00:46 +0100

    progress bar works, time progress works, volume slider works but is misplaced

--- frontend/src/views/podcasts/SinglePodcastItemView.vue
+++ frontend/src/views/podcasts/SinglePodcastItemView.vue
@@ -1,30 +1,62 @@
 <template>
   <v-row align="center" justify="center">
     <v-col cols="8">
-      <v-card
-        ><v-card-title>{{ getPodcastItemTitle }}</v-card-title>
+      <v-card>
         <div class="d-flex flex-no-wrap justify-space-between">
           <v-col cols="10">
+            <v-card-title class="text-truncate">{{
+              getPodcastItemTitle
+            }}</v-card-title>
             <v-card-text
               ><Markdown :source="podcastData ? podcastData.Summary : ''" />
             </v-card-text>
-            <v-card-actions
-              ><v-btn
-                :icon="isPlaying ? 'mdi-pause' : 'mdi-play'"
-                :color="isPlaying ? 'secondary' : 'primary'"
-                @click="switchTrackStatus"
-              ></v-btn
-              ><v-btn icon="mdi-stop" color="surface" @click="stopTrack"></v-btn
-              ><v-btn
-                :icon="isMuted ? 'mdi-volume-mute' : 'mdi-volume-high'"
-                @click="muteTrack"
-              ></v-btn
-            ></v-card-actions>
           </v-col>
-          <v-col cols="2" class="d-flex justify-end">
+          <v-col cols="2" class="d-flex justify-end ml-2">
             <v-avatar class="ma-3" size="125" rounded="0">
               <v-img :src="podcastData ? podcastData.Image : ''"></v-img>
             </v-avatar>
+          </v-col>
+        </div>
+        <div>
+          <v-col cols="12">
+            <!--qui mettiamo tutti i controlli del player da spostare in component-->
+            <v-row no-gutters
+              >{{ timePlayed }} <v-spacer></v-spacer> {{ totalTime }}</v-row
+            >
+            <v-row no-gutters class="mt-1">
+              <v-progress-linear
+                v-model="getRelativeProgress"
+                color="primary"
+              ></v-progress-linear
+            ></v-row>
+            <v-row no-gutters justify="center" class="mt-2">
+              <v-card-actions>
+                <v-btn
+                  :icon="isPlaying ? 'mdi-pause' : 'mdi-play'"
+                  :color="isPlaying ? 'info' : 'primary'"
+                  @click="switchTrackStatus"
+                ></v-btn
+                ><v-btn
+                  icon="mdi-stop"
+                  color="secondary"
+                  class="mx-1"
+                  @click="stopTrack"
+                ></v-btn
+                ><v-btn
+                  :icon="isMuted ? 'mdi-volume-mute' : 'mdi-volume-high'"
+                  @click="muteTrack"
+                ></v-btn>
+              </v-card-actions>
+              <v-slider
+                v-model="podcastVolume"
+                :min="0"
+                :max="1"
+                :step="0.05"
+                color="primary"
+                thumb-label
+                @update:model-value="setTrackVolume()"
+              ></v-slider>
+            </v-row>
           </v-col>
         </div>
       </v-card>
@@ -35,6 +67,7 @@
 <script>
 import { getPodcastItemById } from "@/api";
 import { Howl, Howler } from "howler";
+import { intervalToDuration, formatDuration } from "date-fns";
 import Markdown from "vue3-markdown-it";
 export default {
   components: {
@@ -45,10 +78,55 @@ export default {
     howlerData: null,
     isPlaying: false,
     isMuted: false,
+    podcastProgress: 0,
+    podcastVolume: 0.5,
+    timeTrackerId: null,
   }),
   computed: {
     getPodcastItemTitle() {
       return this.podcastData ? this.podcastData.Title : "";
+    },
+    getRelativeProgress() {
+      if (!this.podcastData) return 0;
+      return (this.podcastProgress / this.podcastData.Duration) * 100;
+    },
+    timePlayed() {
+      if (!this.howlerData) return "00:00";
+      const duration = intervalToDuration({
+        start: 0,
+        end: this.podcastProgress * 1000,
+      });
+      const zeroPad = (num) => String(num).padStart(2, "0");
+
+      return formatDuration(duration, {
+        format: ["minutes", "seconds"],
+        // format: ["hours", "minutes", "seconds"],
+        zero: true,
+        delimiter: ":",
+        locale: {
+          formatDistance: (_token, count) => zeroPad(count),
+        },
+      });
+    },
+    totalTime() {
+      if (!this.podcastData) return 0;
+
+      const duration = intervalToDuration({
+        start: 0,
+        end: this.podcastData.Duration * 1000,
+      });
+
+      const zeroPad = (num) => String(num).padStart(2, "0");
+
+      return formatDuration(duration, {
+        format: ["minutes", "seconds"],
+        // format: ["hours", "minutes", "seconds"],
+        zero: true,
+        delimiter: ":",
+        locale: {
+          formatDistance: (_token, count) => zeroPad(count),
+        },
+      });
     },
   },
   async created() {
@@ -66,12 +144,15 @@ export default {
         this.podcastData = response.data.data;
         let howlerConfig = {
           src: [this.podcastData.FileURL],
-          volume: 0.5,
+          volume: this.podcastVolume,
           loop: false,
           html5: true,
-          preload: "metadata",
+          preload: true,
         };
         this.howlerData = new Howl(howlerConfig);
+        this.timeTrackerId = setInterval(() => {
+          this.podcastProgress = this.howlerData.seek();
+        }, 1000);
       });
     },
     switchTrackStatus() {
@@ -95,6 +176,9 @@ export default {
     muteTrack() {
       this.isMuted ? Howler.mute(false) : Howler.mute(true);
       this.isMuted = !this.isMuted;
+    },
+    setTrackVolume() {
+      this.howlerData.volume(this.podcastVolume);
     },
   },
 };
