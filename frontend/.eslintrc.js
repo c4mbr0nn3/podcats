c9b2847fec53613c71d47ab6c8580a058d30c49f
Francesco Zorzi <frensinik89@yahoo.it>
2023-03-11 12:23:32 +0100

    align deps to latest vue3 init command

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,10 +1,17 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
+  root: true,
   env: {
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
-  rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier/skip-formatting",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
   },
 };
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,21 @@
 // Plugins
 import vue from "@vitejs/plugin-vue";
-import vuetify from "vite-plugin-vuetify";
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 // Utilities
 import { defineConfig } from "vite";
-import eslintPlugin from "vite-plugin-eslint";
 import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: { transformAssetUrls },
+    }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
     vuetify({
       autoImport: true,
     }),
-    eslintPlugin(),
   ],
   define: { "process.env": {} },
   build: {
@@ -26,8 +26,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
     extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
-  },
-  server: {
-    port: 3000,
   },
 });
