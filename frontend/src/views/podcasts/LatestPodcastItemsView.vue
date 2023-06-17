48ba03ab65ee89e4899d16026dff48f2c907193c
Francesco Zorzi <frensinik89@yahoo.it>
2023-06-18 01:51:36 +0200

    not perfect

--- frontend/src/views/podcasts/LatestPodcastItemsView.vue
+++ frontend/src/views/podcasts/LatestPodcastItemsView.vue
@@ -1,19 +1,36 @@
 <template>
   <v-row justify="center">
     <v-col cols="9">
+      <v-card>
+        <v-card-title>Latest Episodes</v-card-title>
+
+        <v-card-text v-if="podcastItemList.length > 0">
+          <div v-for="(podcastItem, id) in podcastItemList" :key="id">
+            <v-lazy
+              :options="{
+                threshold: 0.25,
+              }"
+              transition="fade-transition"
+              min-height="220"
+              class="mt-3"
+            >
+              <SinglePodcastItemCard
+                :podcast-id="podcastItem.PodcastID"
+                :podcast-item="podcastItem"
+                @change-fav-status="changeFavStatus"
+                @change-played-status="changePlayedStatus"
+              />
+            </v-lazy>
+          </div>
+        </v-card-text>
+      </v-card>
       <v-card
-        ><v-card-title>Latest Episodes</v-card-title>
-        <v-card-text v-if="podcastItemsData.length > 0">
-          <SinglePodcastItemCard
-            v-for="(podcastItem, index) in podcastItemsData"
-            :key="index"
-            class="mt-3"
-            :podcast-id="podcastItem.PodcastID"
-            :podcast-item="podcastItem"
-            @change-fav-status="changeFavStatus"
-            @change-played-status="changePlayedStatus"
-          /> </v-card-text></v-card
-      ><v-card v-intersect="onIntersect"></v-card>
+        v-if="!isLoading || !isFetchingNextPage || !hasNextPage"
+        ref="sentinel"
+      />
+      <v-row v-if="isLoading || isFetchingNextPage" justify="center">
+        <v-col class="d-flex justify-center"> Loading... </v-col>
+      </v-row>
     </v-col>
   </v-row>
 </template>
@@ -21,40 +38,70 @@
 <script setup>
 import { PodcastItemsService } from "@/api";
 import { useInfiniteQuery } from "@tanstack/vue-query";
-import { ref } from "vue";
 import SinglePodcastItemCard from "@/components/SinglePodcastItemCard.vue";
+import { computed, ref } from "vue";
+import { useIntersectionObserver } from "@vueuse/core";
+import { watch } from "vue";
 
-const podcastItemsData = ref([]);
-const currentPage = ref(1);
-const nextPage = ref(null);
-const pageCount = ref(null);
+const sentinel = ref(null);
+const sentinelVisible = ref(false);
 
-async function fetchData(pageId) {
-  await PodcastItemsService.getLatestPodcastItems(pageId).then((response) => {
-    if (response.data.podcastItems.length > 0) {
-      response.data.podcastItems.forEach((item) =>
-        podcastItemsData.value.push(item)
-      );
-    }
-    currentPage.value = response.data.thisPage;
-    pageCount.value = response.data.pageCount;
-    nextPage.value = response.data.nextPage;
+const { data, fetchNextPage, hasNextPage, isLoading, isFetchingNextPage } =
+  useInfiniteQuery({
+    queryKey: ["latest-podcast-items"],
+    queryFn: fetchData,
+    getNextPageParam: (lastPage) => {
+      return lastPage.nextCursor;
+    },
   });
+
+watch(
+  () => sentinelVisible.value,
+  (value) => {
+    if (value) {
+      onIntersect();
+    }
+  }
+);
+
+const podcastItemList = computed(() => {
+  if (!data.value) return [];
+  return data.value.pages.reduce((acc, page) => {
+    return [...acc, ...page.data];
+  }, []);
+});
+
+async function fetchData({ pageParam = 1 }) {
+  return await PodcastItemsService.getLatestPodcastItems(pageParam).then(
+    (response) => {
+      return {
+        data: response.data.podcastItems,
+        nextCursor: response.data.nextPage ? response.data.nextPage : undefined,
+      };
+    }
+  );
 }
 
-// TODO: fix doppia chiamata non voluta...
 async function onIntersect() {
-  if (currentPage.value == pageCount.value || !nextPage.value) return;
-  await fetchData(nextPage.value);
+  console.log("onIntersect");
+  if (!hasNextPage.value) return;
+  fetchNextPage();
 }
 
 function changePlayedStatus(event) {
-  let item = podcastItemsData.value.find((el) => el.ID == event);
-  item.IsPlayed = !item.IsPlayed;
+  console.log("changePlayedStatus", event);
+  // let item = podcastItemsData.value.find((el) => el.ID == event);
+  // item.IsPlayed = !item.IsPlayed;
 }
 
 function changeFavStatus(event) {
-  let item = podcastItemsData.value.find((el) => el.ID == event.id);
-  item.BookmarkDate = event.bookmarkDate;
+  console.log("changeFavStatus", event);
+
+  // let item = podcastItemsData.value.find((el) => el.ID == event.id);
+  // item.BookmarkDate = event.bookmarkDate;
 }
+
+useIntersectionObserver(sentinel, ([{ isIntersecting }]) => {
+  sentinelVisible.value = isIntersecting;
+});
 </script>
