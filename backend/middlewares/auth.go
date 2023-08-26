7b3ff6b28559c9a4f205acb9ff752afd96812d18
Francesco Zorzi <frensinik89@yahoo.it>
2023-08-26 16:52:41 +0000

    fix: better handling of response with error

--- backend/middlewares/auth.go
+++ backend/middlewares/auth.go
@@ -11,12 +11,12 @@ func Auth() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		authToken := ctx.GetHeader("Authorization")
 		if authToken == "" {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized", "message": "No token provided"})
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized, no token provided"})
 			return
 		}
 		err := auth.ValidateJwtToken(authToken)
 		if err != nil {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized", "message": err.Error()})
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized " + err.Error()})
 			return
 		}
 		ctx.Next()
--- frontend/src/api/axios/index.js
+++ frontend/src/api/axios/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useAuthStore } from "@/stores/auth";
+import { useRouter } from "vue-router";
+import { showErrorSnackbar } from "@/utils/snackbar";
 
 const baseURL = import.meta.env.DEV
   ? "http://localhost:8000/api/v1"
@@ -25,9 +27,30 @@ axiosClient.interceptors.request.use((config) => {
 axiosClient.interceptors.response.use(
   (res) => res,
   (err) => {
-    console.error("Api Error:", err);
+    const authStore = useAuthStore();
+    const router = useRouter();
+    const messager = err.response.data?.error || getGenericMessage(err);
+    showErrorSnackbar(messager);
+    if (err.response.status === 401) authStore.logout();
+    if (err.response.status === 403) router.push({ name: "home" });
     return Promise.reject(err);
   }
 );
+
+function getGenericMessage(err) {
+  const status = err.response.status;
+  switch (status) {
+    case 401:
+      return "Unauthorized, please login";
+    case 403:
+      return "Forbidden, you don't have permission to access this resource";
+    case 404:
+      return "Not Found, resource not found";
+    case 500:
+      return "Internal Server Error, something went wrong";
+    default:
+      return "Something went wrong, please try again later";
+  }
+}
 
 export default axiosClient;
