d88220d48d456e9c6a30e1718d6214a287fb5204
Francesco Zorzi <frensinik89@yahoo.it>
2023-11-25 09:12:50 +0100

    style: eslint vue custom event rule changed to kebab-case

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,3 +1,11 @@
 import antfu from '@antfu/eslint-config'
 
-export default await antfu()
+export default await antfu({
+  overrides: {
+    vue: {
+      'vue/custom-event-name-casing': ['error', 'kebab-case', {
+        ignores: [],
+      }],
+    },
+  },
+})
